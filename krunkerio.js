46169d9f6bba0ec3437820748872119b88a28640
AlexTabur <30406367+AlexTabur@users.noreply.github.com>
2020-08-02 15:22:23 +0300

    Update krunkerio.js

--- krunkerio.js
+++ krunkerio.js
@@ -8,31 +8,13 @@ ev3stop = new KeyboardEvent("keyup",{bubbles: true, composed:true,key:" ", code:
 aim=function(el){el.dispatchEvent(ev);},
 lastTimeWalked=0,
 lastTimeJumped=0,
-walk=function(){document.getElementsByTagName("html")[0].dispatchEvent(ev2);lastTimeWalked=tNow},
-stopwalk=function(){document.getElementsByTagName("html")[0].dispatchEvent(ev2stop)},
-jump = function(){document.getElementsByTagName("html")[0].dispatchEvent(ev3);lastTimeJumped=tNow;},//setTimeout(document.getElementsByTagName("html")[0].dispatchEvent(ev5),100)},
-stopjump=function(){document.getElementsByTagName("html")[0].dispatchEvent(ev3stop)},
-squat = function(){document.getElementsByTagName("html")[0].dispatchEvent(ev4);},
+walk=function(){document.getElementsByTagName("body")[0].dispatchEvent(ev2);},
+stopwalk=function(){document.getElementsByTagName("body")[0].dispatchEvent(ev2stop)},
+jump = function(){document.getElementsByTagName("body")[0].dispatchEvent(ev3);},
+stopjump=function(){document.getElementsByTagName("body")[0].dispatchEvent(ev3stop)},
+squat = function(){document.getElementsByTagName("body")[0].dispatchEvent(ev4);},
 simulate = function () {
-    //cnvalex=document.getElementsByTagName("canvas");
-    //for (var i = 0; i < cnvalex.length; i++) {
-        //aim(cnvalex[i]);
-    //}
-    //console.log(tNow);
-    //squat();
-    
     walk();
-    //console.log(window.performance.now());
-    //if(window.performance.now()-lastTimeJumped>=500){jump()};
-    //console.log("simulating")
 };
-window.main = function () {
-    tNow=window.performance.now();
-	simulate();
-    //jump();
-    window.requestAnimationFrame(main);
-    //window.requestAnimationFrame(jump);
-    tAfter=tNow
-}
-window.onload=function(){//jump();walk();//setInterval(jump(),500);
-window.main();tNow=window.performance.now()}
+
+window.onload=function(){setInterval(squat(),500)}
